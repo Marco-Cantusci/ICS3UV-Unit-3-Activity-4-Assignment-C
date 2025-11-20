efbf160771869f365840bcdf6e4818a5c7bacb1d
default <marco.cantusci@stu.ocsb.ca>
2025-11-20 17:04:25 +0000

    Finished C

---
+++ C.go
@@ -0,0 +1,64 @@
+/*
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-11-19
+ * @fileoverview If statement problem.
+ */
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	// declare variables
+	var name string
+	var ageString string
+	var ageNumber int
+	var student string
+
+	reader := bufio.NewReader(os.Stdin)
+
+	// input
+
+	// ask for name
+	fmt.Print("What is your name? \n")
+	name, _ = reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+
+	// ask for age
+	fmt.Print("How old are you? \n")
+	ageString, _ = reader.ReadString('\n')
+	ageString = strings.TrimSpace(ageString)
+	ageNumber, _ = strconv.Atoi(ageString)
+
+	// ask if they are a student
+	fmt.Print("Are you a student? \n")
+	student, _ = reader.ReadString('\n')
+	student = strings.TrimSpace(student)
+
+	// processing
+if strings.ToLower(student) == "yes" {
+	if ageNumber >= 13 && ageNumber <= 19 {
+		fmt.Printf("Student %s is a teenager. \n", name)
+	} else if ageNumber >= 5 && ageNumber <= 12 {
+		fmt.Printf("Student %s is a child. \n", name)
+	} else {
+		fmt.Printf("%s is in a different life stage. \n", name)
+	}
+} else if strings.ToLower(student) == "no" {
+	if ageNumber >= 20 && ageNumber <= 30 {
+		fmt.Printf("%s is a young adult. \n", name)
+	} else {
+	fmt.Printf("%s is in a different life stage. \n", name)
+	}
+}
+
+fmt.Println("\nDone.")
+}
---
+++ C.ts
@@ -0,0 +1,34 @@
+/*
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-11-19
+ * @fileoverview If statement problem.
+ */
+
+// input
+const NAME: string = prompt("What is your name? \n") || ("n/a");
+
+const ageString: string = prompt("How old are you? \n") || ("0");
+const ageNumber: number = parseInt(ageString);
+
+const STUDENT: string = prompt("Are you a student? \n") || ("no");
+
+// processing
+
+if (STUDENT.toLowerCase() == "yes") {
+  if (ageNumber >= 13 && ageNumber <= 19) {
+    console.log(`Student ${NAME} is a teenager. \n`);
+  } else if (ageNumber >= 5 && ageNumber <= 12) {
+    console.log(`Student ${NAME} is a child. \n`);
+  } else {
+    console.log(`${NAME} is in a different life stage. \n`);
+  }
+} else if (STUDENT.toLowerCase() == "no") {
+  if (ageNumber >= 20 && ageNumber <= 30) {
+    console.log(`${NAME} is a young adult.`);
+  } else {
+    console.log(`${NAME} is in a different life stage.`);
+  }
+}
+
+console.log("\nDone.")
